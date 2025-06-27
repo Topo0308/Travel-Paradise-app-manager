d3c5b7d1334430f9ebd80567cddfc6c84cb0b37e
luc-007 <kemtaluc23@gmail.com>
2025-06-27 12:39:29 +0200

    Revert "migration de la base de donnée"
    This reverts commit 63b07b053a618d54c0465f47c07a8536f69c3511.

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -10,5 +10,4 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
